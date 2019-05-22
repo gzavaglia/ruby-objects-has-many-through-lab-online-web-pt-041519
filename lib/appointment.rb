a85185f2b254aea7e32f7bbe2e854e1c211b1d0f
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-05-22 14:11:07 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,10 +2,12 @@ require_relative "../lib/doctor.rb"
 require_relative "../lib/patient.rb"
 
 class Appointment
-  attr_accessor :date, :doctor, :appointment
+  attr_accessor :date, :doctor, :patient
   @@appts = []
-  def initialize(name)
+  def initialize(date, patient, doctor)
     @date = date
+    @doctor = doctor
+    @patient = patient
     @@appts << self
   end
 end
